b76dd946e62e9d368f48489695d76ff64401a2bf
Ursulashrestha <scrappermoon1999@gmail.com>
2025-03-30 09:36:36 +0200

    resolving npm isue with custom domain

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import { createApp } from './node_modules/vue/dist/vue.esm-bundler.js'
+import { createApp } from "vue";
 import App from './App.vue'
 import router from './router'
 
