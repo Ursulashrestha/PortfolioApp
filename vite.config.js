a5668ad8ee121d041c6a2285494fdf738b8e6540
Ursulashrestha <scrappermoon1999@gmail.com>
2025-03-31 11:36:59 +0200

    fixing custom domain issue

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.NODE_ENV === 'production' ? './' : '/',  // base set to root to support custom domain.
+	base: '', // Empty base for GitHub Pages custom domain
 	plugins: [vue()],
 	resolve: {
 		alias: {
-		'@': fileURLToPath(new URL('./src', import.meta.url))
+			'@': fileURLToPath(new URL('./src', import.meta.url))
+		}
+	},
+	build: {
+		rollupOptions: {
+			external: []
 		}
 	}
 })
