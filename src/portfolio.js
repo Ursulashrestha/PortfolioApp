4c685eec3c9cbcb5455d80a81ce79cac6be43f12
Ursulashrestha <scrappermoon1999@gmail.com>
2025-04-01 16:28:22 +0200

    changing favicons

--- src/portfolio.js
+++ src/portfolio.js
@@ -24,7 +24,7 @@ const greeting = {
         link: "images/initials.png",
         custom: true 
     },
-    portraitLink: 'images/that.jpeg'
+    portraitLink: "images/that.jpeg"
 }
 
 const socialMediaLinks = {
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-	base: '', // Empty base for GitHub Pages custom domain
+	base: '/', // base set to root to support custom domain.
 	plugins: [vue()],
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
-		}
-	},
-	build: {
-		rollupOptions: {
-			external: []
+		'@': fileURLToPath(new URL('./src', import.meta.url))
 		}
 	}
 })
